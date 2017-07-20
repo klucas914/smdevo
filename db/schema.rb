fd9c01a7a631043a7ee4dd125f9c7f546ff88854
Kristin Lucas <klucas914@gmail.com>
2017-07-20 16:09:40 +1000

    generated organization model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717015051) do
+ActiveRecord::Schema.define(version: 20170720060741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20170717015051) do
     t.text     "entry"
     t.integer  "user_id"
     t.index ["user_id"], name: "index_journals_on_user_id", using: :btree
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
